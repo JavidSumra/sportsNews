10a378c51bf1bb5404006371cf222ef38bc83688
JavidSumra <javidsumra135@gmail.com>
2023-11-07 07:34:27 +0530

    Updating UI Modal For Read Article

--- src/pages/LiveNews/ReadArticle.tsx
+++ src/pages/LiveNews/ReadArticle.tsx
@@ -1,25 +1,25 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/restrict-template-expressions */
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, Fragment } from "react";
 import { Transition, Dialog } from "@headlessui/react";
 import { useNavigate, useParams } from "react-router-dom";
 import { API_ENDPOINT } from "../../config/constants";
-import { XMarkIcon } from "@heroicons/react/24/outline";
 import { NewsData } from "../../context/News/types";
 import Skeleton from "react-loading-skeleton";
+import { Team } from "../../context/Teams/types";
 
-type ArticleDetail = NewsData & {
-  content: string;
+type dataDetail = NewsData & {
+  content?: string;
 };
 
-const ReadArticle = () => {
+const Readdata = () => {
   const navigate = useNavigate();
   const { Id } = useParams();
 
-  const [data, setData] = useState<ArticleDetail>();
+  const [data, setData] = useState<dataDetail>();
   const [isOpen, setIsOpen] = React.useState<boolean>(true);
 
-  // FetchMatchData retrieve Data of Particular Article When User Clicks on Readmore
+  // FetchMatchData retrieve Data of Particular data When User Clicks on Readmore
   const FetchMatchData = async () => {
     const res = await fetch(`${API_ENDPOINT}/articles/${Id}`, {
       method: "GET",
@@ -27,7 +27,7 @@ const ReadArticle = () => {
         "Content-Type": "application/json",
       },
     });
-    const data: ArticleDetail = await res.json();
+    const data: dataDetail = await res.json();
     setData(data);
   };
 
@@ -39,27 +39,26 @@ const ReadArticle = () => {
     setIsOpen(false);
     navigate("/dashboard");
   }
-
   if (data) {
     return (
       <>
-        <Transition appear show={isOpen} as={React.Fragment}>
+        <Transition appear show={isOpen} as={Fragment}>
           <Dialog as="div" className="relative z-10" onClose={closeModal}>
             <Transition.Child
-              as={React.Fragment}
-              enter="ease-out duration-50"
+              as={Fragment}
+              enter="ease-out duration-300"
               enterFrom="opacity-0"
               enterTo="opacity-100"
-              leave="ease-in duration-100"
+              leave="ease-in duration-200"
               leaveFrom="opacity-100"
               leaveTo="opacity-0"
             >
               <div className="fixed inset-0 bg-black bg-opacity-25" />
             </Transition.Child>
-            <div className="fixed inset-0 overflow-y-auto">
-              <div className="flex min-h-full items-center justify-center p-4 text-center">
+            <div className="fixed inset-0 overflow-y-auto custom-scrollbar">
+              <div className="flex min-h-full items-center justify-center sm:p-12 text-center">
                 <Transition.Child
-                  as={React.Fragment}
+                  as={Fragment}
                   enter="ease-out duration-300"
                   enterFrom="opacity-0 scale-95"
                   enterTo="opacity-100 scale-100"
@@ -67,52 +66,76 @@ const ReadArticle = () => {
                   leaveFrom="opacity-100 scale-100"
                   leaveTo="opacity-0 scale-95"
                 >
-                  <Dialog.Panel className="w-full max-w-screen-sm transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all dark:text-white dark:bg-slate-900">
+                  <Dialog.Panel className="w-full transform overflow-hidden rounded-2xl bg-white p-4 md:p-6 text-left align-middle shadow-xl transition-all">
+                    <div className="absolute right-0 top-0 pr-4 pt-4 ">
+                      <button
+                        type="button"
+                        className=" text-xl font-semibold rounded-md bg-white text-gray-400 hover:text-gray-500"
+                        onClick={closeModal}
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                          fill="currentColor"
+                          className="w-6 h-6 md:text-3xl text-md font-bold"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z"
+                            clipRule="evenodd"
+                          />
+                        </svg>
+                      </button>
+                    </div>
                     <Dialog.Title
                       as="h3"
-                      className="text-lg font-medium leading-6 text-gray-900 "
-                    >
-                      <div className="flex items-center justify-between mb-3">
-                        <div className="font-bold text-lg dark:text-white">
-                          {data.title}
+                      className="text-lg font-light leading-6 text-gray-700/75"
+                    ></Dialog.Title>
+                    <div className="mt-4">
+                      <div className="bg-white">
+                        <div className="mx-auto max-w-7xl md:px-8">
+                          <div className="mx-auto flex max-w-2xl flex-col items-end justify-between gap-16 lg:mx-0 lg:max-w-none lg:flex-row">
+                            <div className="w-full lg:max-w-lg lg:flex-auto">
+                              <h2 className="text-xl font-bold tracking-tight text-gray-900 md:text-4xl">
+                                {data?.sport?.name}
+                              </h2>
+                              <p className="text-sm font-semibold mt-2 ">
+                                {
+                                  String(data?.date)
+                                    ?.toString()
+                                    ?.split("T")[1]
+                                    ?.split(".")[0]
+                                }
+                                ,{String(data?.date)?.toString()?.split("T")[0]}
+                              </p>
+                              <p className="mt-6 text-xl leading-8 text-gray-600">
+                                {data?.summary}
+                              </p>
+                              <img
+                                src={data?.thumbnail}
+                                className="mt-16 aspect-[6/5] w-full rounded-2xl bg-gray-50 object-cover lg:aspect-auto lg:h-[34.5rem]"
+                              />
+                            </div>
+                            <div className="w-full lg:max-w-xl lg:flex-auto">
+                              <h3 className="text-xl md:text-3xl font-bold">
+                                {data?.title}
+                              </h3>
+                              <p className="my-3 space-x-2">
+                                {data?.teams?.map((team: Team) => (
+                                  <span
+                                    className="bg-gray-200/75 text-gray-600/75 p-2 my-2"
+                                    key={team?.id}
+                                  >
+                                    # {team?.name}
+                                  </span>
+                                ))}
+                              </p>
+                              <p className="my-6 text-md md:text-xl leading-normal md:leading-8 text-gray-600">
+                                {data?.content}
+                              </p>
+                            </div>
+                          </div>
                         </div>
-                        <button
-                          onClick={closeModal}
-                          className="inline-flex justify-center "
-                        >
-                          <XMarkIcon className="w-6 h-6 text-black dark:text-white" />
-                        </button>
-                      </div>
-                    </Dialog.Title>
-                    <div className="mt-2">
-                      <img
-                        src={data.thumbnail}
-                        className="w-full h-[180px] object-cover rounded-lg"
-                        alt="Thumbnail"
-                      />
-                      <div className="text-lg text-left font-medium mt-2 flex justify-between">
-                        <div>Sport Type : {data.sport.name}</div>
-                        <div className="mx-4">
-                          {new Date(data.date).toUTCString().split("", 16)}
-                        </div>
-                      </div>
-                      <div
-                        className={`text-lg flex text-left font-medium mt-2 ${
-                          data.teams.length > 0 ? "" : "hidden"
-                        }`}
-                      >
-                        <div className="mr-2"> Playing Teams :</div>
-                        <ul typeof="1">
-                          {data.teams.map((team, index) => (
-                            <li key={team.id}>
-                              ({index + 1}) {team.name}
-                            </li>
-                          ))}
-                        </ul>
-                      </div>
-                      <hr className="m-2" />
-                      <div className="overflow-y-auto text-lg font-medium mt-4 h-[300px]">
-                        {data.content}
                       </div>
                     </div>
                   </Dialog.Panel>
@@ -149,37 +172,62 @@ const ReadArticle = () => {
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                <Dialog.Panel className="w-full max-w-screen-sm transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all dark:bg-slate-900">
+                <Dialog.Panel className="w-full max-w-screen-lg transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all dark:bg-slate-900">
+                  <div className="absolute right-0 top-0 pr-4 pt-4 ">
+                    <button
+                      type="button"
+                      className=" text-xl font-semibold rounded-md bg-white text-gray-400 hover:text-gray-500"
+                      onClick={closeModal}
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="currentColor"
+                        className="w-6 h-6 md:text-3xl text-md font-bold"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                    </button>
+                  </div>
                   <Dialog.Title
                     as="h3"
                     className="text-lg font-medium leading-6 text-gray-900"
-                  >
-                    <div className="flex items-center justify-between mb-3">
-                      <div className="font-bold text-lg">
-                        <Skeleton height={20} width={550} />
+                  ></Dialog.Title>
+                  <div className="mt-4">
+                    <div className="bg-white">
+                      <div className="mx-auto max-w-7xl md:px-8">
+                        <div className="mx-auto flex max-w-2xl flex-col items-end justify-between gap-16 lg:mx-0 lg:max-w-none lg:flex-row">
+                          <div className="w-full lg:max-w-lg lg:flex-auto">
+                            <h2 className="text-xl font-bold tracking-tight text-gray-900 md:text-4xl">
+                              <Skeleton height={30} width={150} />
+                            </h2>
+                            <p className="text-sm font-semibold mt-2 ">
+                              <Skeleton height={15} width={80} />
+                            </p>
+                            <p className="mt-6 text-xl leading-8 text-gray-600">
+                              <Skeleton height={50} width={300} />
+                            </p>
+                            <Skeleton className="mt-16 aspect-[6/5] w-full rounded-2xl bg-gray-50 object-cover lg:aspect-auto lg:h-[34.5rem]" />
+                          </div>
+                          <div className="w-full lg:max-w-xl lg:flex-auto">
+                            <h3 className="text-xl md:text-3xl font-bold">
+                              <Skeleton height={40} width={450} />
+                            </h3>
+                            <p className="my-3 space-x-2">
+                              <span className="bg-gray-200/75 text-gray-600/75 p-2 my-2">
+                                <Skeleton height={8} width={80} />
+                              </span>
+                            </p>
+                            <p className="my-6 text-md md:text-xl leading-normal md:leading-8 text-gray-600">
+                              <Skeleton height={435} width={500} />
+                            </p>
+                          </div>
+                        </div>
                       </div>
-                      <button
-                        onClick={closeModal}
-                        className="inline-flex justify-center "
-                      >
-                        <XMarkIcon className="w-6 h-6 text-black dark:text-white" />
-                      </button>
-                    </div>
-                  </Dialog.Title>
-                  <div className="mt-2">
-                    <Skeleton className="w-full h-[180px] object-cover rounded-lg" />
-                    <div className="text-lg text-left font-medium mt-2 flex justify-between">
-                      <div>
-                        <Skeleton height={20} width={100} />
-                      </div>
-                      <div className="mx-4">
-                        <Skeleton height={20} width={100} />
-                      </div>
-                    </div>
-
-                    <hr className="m-2" />
-                    <div className="overflow-y-auto text-lg font-medium mt-4 h-[300px]">
-                      <Skeleton height={18} width={580} count={10} />
                     </div>
                   </div>
                 </Dialog.Panel>
@@ -192,4 +240,4 @@ const ReadArticle = () => {
   }
 };
 
-export default ReadArticle;
+export default Readdata;
